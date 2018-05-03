9424ea8d1cb9f7a4cad390353f8a3a1a3a39b996
Nagy Olivér <nagyoliver777@gmail.com>
2018-05-03 13:47:37 +0000

    Update bot.js

--- bot.js
+++ bot.js
@@ -3,7 +3,8 @@ const Discord = require('discord.js');
 var bot = new Discord.Client({});
 var random;
 
-var promises = ["Ingyen sör", "Adócsökkentés", "Örökélet +20 év", "Pénzosztást", "Legyen minden jobb!", "Ne kelljen dolgozni, de legyen pénz!", "Marilizáljuk a Legahuánát!", "Több mindent, kevesebb semmit!", "Választóink 100 százaléka ránk szavaz", "93 százalék hogy nem fogunk lopni", "2018 a kutya éve", "Olyan aranyos, biztos nem akar lopni...", "Te már választottál magadnak kutyapárt?"];
+var promises = ["Ingyen sör", "Adócsökkentés", "Örökélet +20 év", "Pénzosztást", "Legyen minden jobb!", "Ne kelljen dolgozni, de legyen pénz!", "Marilizáljuk a Legahuánát!", "Több mindent, kevesebb semmit!", "Választóink 100 százaléka ránk szavaz", "93 százalék hogy nem fogunk lopni", "2018 a kutya éve", "Olyan aranyos, biztos nem akar lopni...", "Te már választottál magadnak kutyapárt?", "Államcsőd eladó!"];
+var images = [""];
 
 bot.on("ready", function () {
     console.log("Ready");
@@ -14,7 +15,7 @@ bot.on("message", function (message) {
 	
 	if (message.content == "!kellakutya") {
 		random = Math.floor((Math.random() * 14) + 0);
-		message.channel.send(promises[random]);
+		message.channel.send(promises[random] { file: ["http://civilhirugynokseg.hu/wp-content/uploads/2015/07/MKKP-620x270.jpg"] });
 	}
 });
 
