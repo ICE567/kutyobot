6929c73ebd65efea723a05e4558a1e161158a4a9
Nagy Olivér <nagyoliver777@gmail.com>
2018-05-03 13:15:37 +0000

    Update bot.js

--- bot.js
+++ bot.js
@@ -3,7 +3,7 @@ const Discord = require('discord.js');
 var bot = new Discord.Client({});
 var random;
 
-var promises = ["Ingyen sör", "Adócsökkentés", "Örökélet +20 év", "Pénzosztást", "Legyen minden jobb!", "Ne kelljen dolgozni, de legyen pénz!", "Marilizáljuk a Legahuánát!", "Több mindent, kevesebb semmit!", "Szavazóink 100 százaléka ránk szavaz", "93 százalék hogy nem fogunk lopni", "2018 a kutya éve", "Olyan aranyos, biztos nem akar lopni...", "Te már választottál magadnak kutyapárt?"];
+var promises = ["Ingyen sör", "Adócsökkentés", "Örökélet +20 év", "Pénzosztást", "Legyen minden jobb!", "Ne kelljen dolgozni, de legyen pénz!", "Marilizáljuk a Legahuánát!", "Több mindent, kevesebb semmit!", "Választóink 100 százaléka ránk szavaz", "93 százalék hogy nem fogunk lopni", "2018 a kutya éve", "Olyan aranyos, biztos nem akar lopni...", "Te már választottál magadnak kutyapárt?"];
 
 bot.on("ready", function () {
     console.log("Ready");
