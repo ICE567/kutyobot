a229f554232313f7c1572b7b2f54fb80358ee1e1
Nagy Olivér <nagyoliver777@gmail.com>
2018-05-03 13:12:43 +0000

    Update bot.js

--- bot.js
+++ bot.js
@@ -3,7 +3,7 @@ const Discord = require('discord.js');
 var bot = new Discord.Client({});
 var random;
 
-var promises = ["Ingyen sör", "Adócsökkentés", "Örökélet +20 év", "Pénzosztást", "Legyen minden jobb!", "Ne kelljen dolgozni, de legyen pénz!", "Marilizáljuk a Legahuánát!", "Több mindent, kevesebb semmit!"];
+var promises = ["Ingyen sör", "Adócsökkentés", "Örökélet +20 év", "Pénzosztást", "Legyen minden jobb!", "Ne kelljen dolgozni, de legyen pénz!", "Marilizáljuk a Legahuánát!", "Több mindent, kevesebb semmit!", "Szavazóink 100 százaléka ránk szavaz", "93 százalék hogy nem fogunk lopni", "2018 a kutya éve", "Olyan aranyos, biztos nem akar lopni...", "Te már választottál magadnak kutyapárt?"];
 
 bot.on("ready", function () {
     console.log("Ready");
@@ -13,7 +13,7 @@ bot.on("message", function (message) {
     if (message.author.equals(bot.user)) return;
 	
 	if (message.content == "!kellakutya") {
-		random = Math.floor((Math.random() * 7) + 0);
+		random = Math.floor((Math.random() * 14) + 0);
 		message.channel.send(promises[random]);
 	}
 });
